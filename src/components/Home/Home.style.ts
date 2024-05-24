b86709a150a616dd05151f8ec716f641800f54b0
bryankiansarmen <sarmenbryankian@gmail.com>
2024-05-24 18:06:31 +0800

    add home page contents

--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -7,3 +7,31 @@ export const DivContainer = styled.div`
   align-items: center;
   height: 100%;
 `;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  gap: 2%;
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid red;
+`;
+
+export const ImageDivContainer = styled.div`
+  border: 1px solid green;
+`;
+
+export const SpanTextContainer = styled.div`
+  text-align: justify;
+  box-sizing: border-box;
+  padding: 50px;
+  border: 1px solid green;
+  width: 500px;
+  & h1 {
+    text-align: center;
+    border: 1px solid blue;
+  }
+`;
+
+export const SAmplediv = styled.div`
+  padding: 10px;
+`;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,32 @@ import * as Styled from "./Home.style";
 const Home = () => {
   return (
     <Styled.DivContainer>
-      <h1>Home Page</h1>
+      <Styled.ContentContainer>
+        <Styled.ImageDivContainer>
+          <img
+            src="https://media.istockphoto.com/id/1335020254/vector/blue-lightning-hit-effect-on-black-background.jpg?s=612x612&w=0&k=20&c=QcrbiX28soWhme64koVvaLruLt-2obap8ZbwNfoE9pQ="
+            alt="Animated cartoon lighting"
+            width={500}
+            height={500}
+          />
+        </Styled.ImageDivContainer>
+        <Styled.SpanTextContainer>
+          <h1>Home Page</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
+            leo ut ante gravida eleifend. Suspendisse potenti. Nam dapibus
+            tellus et dolor egestas, eu euismod nunc sollicitudin. Mauris eget
+            nisl sapien. Aenean et sodales tortor. Aenean nec urna vel felis
+            imperdiet faucibus ut eget libero. Phasellus nisl sapien, pulvinar
+            vitae lacinia quis, finibus eget metus. Nam volutpat odio malesuada
+            tellus sollicitudin, ac sollicitudin ipsum rutrum. Aenean dignissim,
+            sem ac accumsan malesuada, ex nisi ullamcorper mauris, et mattis
+            lorem arcu at orci. Pellentesque habitant morbi tristique senectus
+            et netus et malesuada fames ac turpis egestas.
+          </p>
+        </Styled.SpanTextContainer>
+      </Styled.ContentContainer>
+      <Styled.SAmplediv>sasasa</Styled.SAmplediv>
     </Styled.DivContainer>
   );
 };
