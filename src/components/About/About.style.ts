8c68b9a23db895be940b1b202731ca5f91862e25
bryankiansarmen <sarmenbryankian@gmail.com>
2024-05-31 10:21:22 +0800

    add about section content draft

---
+++ src/components/About/About.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  height: 500px;
+  border: 1px solid red;
+`;
+
+export const ItemWrapper = styled.div`
+  border: 1px solid red;
+`;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,25 @@
 import * as GlobalStyled from "../../global.style";
+import * as Styled from "./About.style";
 
 const About = () => {
   return (
     <GlobalStyled.Container>
       <section id="about">
         <GlobalStyled.SectionHeader>About Section</GlobalStyled.SectionHeader>
+        <Styled.ContentWrapper>
+          <Styled.ItemWrapper>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus rem eos aliquid quo rerum temporibus ipsum distinctio
+            numquam ut omnis placeat, nam sint atque quos dolorem laborum?
+            Rerum, esse dolorem.
+          </Styled.ItemWrapper>
+          <Styled.ItemWrapper>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus rem eos aliquid quo rerum temporibus ipsum distinctio
+            numquam ut omnis placeat, nam sint atque quos dolorem laborum?
+            Rerum, esse dolorem.
+          </Styled.ItemWrapper>
+        </Styled.ContentWrapper>
       </section>
     </GlobalStyled.Container>
   );
