b1a987a7a158093ebb5517b1d9652d087e0c30be
bryankiansarmen <sarmenbryankian@gmail.com>
2024-06-03 20:03:53 +0800

    added stacks item

--- src/components/About/About.style.ts
+++ src/components/About/About.style.ts
@@ -10,5 +10,22 @@ export const ContentWrapper = styled.div`
 `;
 
 export const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 12px;
   border: 1px solid red;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #27272a;
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  color: #ffffff;
+  font-size: 50px;
 `;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,6 @@
 import * as GlobalStyled from "../../global.style";
 import * as Styled from "./About.style";
+import * as Si from "react-icons/si";
 
 const About = () => {
   return (
@@ -8,10 +9,36 @@ const About = () => {
         <GlobalStyled.SectionHeader>About Section</GlobalStyled.SectionHeader>
         <Styled.ContentWrapper>
           <Styled.ItemWrapper>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus rem eos aliquid quo rerum temporibus ipsum distinctio
-            numquam ut omnis placeat, nam sint atque quos dolorem laborum?
-            Rerum, esse dolorem.
+            <Styled.Item>
+              <Si.SiCsharp />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiDotnet />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiKotlin />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiKtor />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiPostgresql />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiMysql />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiMicrosoftsqlserver />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiJavascript />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiTypescript />
+            </Styled.Item>
+            <Styled.Item>
+              <Si.SiNodedotjs />
+            </Styled.Item>
           </Styled.ItemWrapper>
           <Styled.ItemWrapper>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
